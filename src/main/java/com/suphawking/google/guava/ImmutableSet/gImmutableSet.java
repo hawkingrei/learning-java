08334ba70557494ddc6c65019c99d8d96bef0af5
真人 <weizhen.wang@duitang.com>
2016-12-07 17:27:59 +0800

    feat: add ImmutableSet

---
+++ src/main/java/com/suphawking/google/guava/ImmutableSet/gImmutableSet.java
@@ -0,0 +1,19 @@
+package com.suphawking.google.guava.ImmutableSet;
+
+import com.google.common.collect.ImmutableSet;
+
+/**
+ * Created by loveknut on 2016/12/7.
+ */
+public class gImmutableSet {
+  public static void main(String[] args) {
+    ImmutableSet<String> a = ImmutableSet.<String>builder()
+        .add("wwz")
+        .add("wwz")
+        .add("fyl")
+        .build();
+
+
+    System.out.println(a);
+  }
+}
